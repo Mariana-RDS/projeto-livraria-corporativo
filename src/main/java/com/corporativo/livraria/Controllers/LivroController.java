dea6339a374d817940ac339b97d88ab8388b3dc6
“Carolina <cfsm1@discente.ifpe.edu.br>
2025-06-26 01:39:55 -0300

    Adicao de update e delete de Livros

--- src/main/java/com/corporativo/livraria/Controllers/LivroController.java
+++ src/main/java/com/corporativo/livraria/Controllers/LivroController.java
@@ -6,15 +6,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
 import com.corporativo.livraria.Dto.LivroDto;
 import com.corporativo.livraria.Service.LivroService;
-
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-
 
 @Controller
 @RequestMapping("/api")
@@ -23,12 +18,11 @@ public class LivroController {
     @Autowired
     private LivroService livroService;
 
-
-    @PostMapping("/create")
+    @PostMapping("/livros/create")
     public ResponseEntity<LivroDto> createLivro(@RequestBody LivroDto livroDto){
         try {
             LivroDto dto = livroService.create(livroDto);
-            if (dto==null) {
+            if (dto == null) {
                 return new ResponseEntity<>(HttpStatus.CONFLICT);
             }
             return new ResponseEntity<>(dto, HttpStatus.OK);
@@ -41,7 +35,7 @@ public class LivroController {
     public ResponseEntity<Set<LivroDto>> getAllLivros(){
         try {
             Set<LivroDto> dtos = livroService.getAll();
-            if(dtos==null || dtos.isEmpty()){
+            if(dtos == null || dtos.isEmpty()){
                 return new ResponseEntity<>(HttpStatus.NO_CONTENT);
             }
             return new ResponseEntity<>(dtos, HttpStatus.OK);
@@ -49,6 +43,23 @@ public class LivroController {
             return new ResponseEntity<>(null, HttpStatus.INTERNAL_SERVER_ERROR);
         }
     }
-    
 
+    @DeleteMapping("/livros/{id}")
+    public ResponseEntity<Void> deleteLivro(@PathVariable Long id) {
+        boolean deleted = livroService.delete(id);
+        if (deleted) {
+            return ResponseEntity.noContent().build();
+        } else {
+            return ResponseEntity.notFound().build();
+        }
+    }
+
+    @PutMapping("/livros/{id}")
+    public ResponseEntity<LivroDto> updateLivro(@PathVariable Long id, @RequestBody LivroDto livroDto) {
+        LivroDto updatedLivro = livroService.update(id, livroDto);
+        if (updatedLivro == null) {
+            return ResponseEntity.notFound().build();
+        }
+        return ResponseEntity.ok(updatedLivro);
+    }
 }
