dd4a9be984b991ba58faf5482bc5526cd94065ee
mariana-rds <mrds.profissional@gmail.com>
2025-06-27 09:56:58 -0300

    versao com comentarios de Vilmar

--- src/main/java/com/corporativo/livraria/Entities/AutorEntity.java
+++ src/main/java/com/corporativo/livraria/Entities/AutorEntity.java
@@ -27,3 +27,10 @@ public class AutorEntity {
     @ManyToMany(mappedBy = "autores")
     private Set<LivroEntity> livros = new HashSet<>();
 }
+
+//colocar na pasta service o que for de modelo
+//alterar Dto para DTO
+//Renomear Entidade, nome simples
+//olhar para livro service, ajustar uso mapping
+//ajustar try catch no livro service
+//ajustar cascade do delete
